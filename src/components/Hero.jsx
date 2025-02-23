5c4a12365667ee0201218d68a110d8ecccb51d20
bennk0509 <nkakiet21@clc.fitus.edu.vn>
2025-02-23 16:18:20 -0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,25 +3,26 @@ import { ReactTyped } from "react-typed";
 
 const Hero  = () => {
     return (
-        <div style={{backgroundImage: `url(${heroBackground})`}}
-            className="min-h-screen bg-no-repeat bg-center bg-cover">
-                <div className="flex flex-col max-w-[450px] w-full justify-center gap-3">
-                    <h1 className="text-[64px] leading-none" >HT Massage Theraphy</h1>
-                        <div className="text-2xl font-medium flex">
-                            <p>chất lượng đi đầu với các dịch vụ</p>
-                            <ReactTyped className="pl-2 text-[#FFC107]"
-                                strings={[
-                                    "PRIVATE GYM",
-                                    "MASSAGE",
-                                    "FITNESS COATCHING",
-                                ]}
-                                typeSpeed={40}
-                                backSpeed={50}
-                                loop
-                                showCursor={false}
-                            />
-                        </div>
-                </div>
+        <div className="relative">
+            <img src={`${heroBackground}`} className="w-screen">
+            </img>
+            <div className="flex flex-col max-w-[450px] w-full justify-center absolute left-2/5 top-1/3 -translate-x-1/2 -translate-y-1/2 gap-3">
+                <h1 className="text-[64px] leading-none" >HT Massage Theraphy</h1>
+                    <div className="text-2xl font-medium flex">
+                        <p>chất lượng đi đầu với các dịch vụ</p>
+                        <ReactTyped className="pl-2 text-[#FFC107]"
+                            strings={[
+                                "PRIVATE GYM",
+                                "MASSAGE",
+                                "FITNESS COATCHING",
+                            ]}
+                            typeSpeed={40}
+                            backSpeed={50}
+                            loop
+                            showCursor={false}
+                        />
+                    </div>
+            </div>
         </div>
     )
 }
